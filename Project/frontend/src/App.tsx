efed51faa599a63b04ef28f569f1bf1b68700aa9
YamidGT <yagonzlaez@unal.edu.co>
2025-12-08 17:08:16 -0500

    feat: add alerts and moderator panel frontend views

--- Project/frontend/src/App.tsx
+++ Project/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import { Header } from "./Components/Header";
 import {Home} from "./pages/Home.tsx";
@@ -11,30 +11,38 @@ import {MyProducts} from "./pages/MyProducts.tsx";
 import {ProductsBySeller} from "./pages/ProductsBySeller.tsx";
 import ProductDetails from  "./pages/ProductDetails.tsx";
 import {EditPost} from "./pages/EditPost.tsx";
+import PanelDeModerador from "./pages/PanelDeModerador.tsx";
+import Alerts from "./pages/Alerts.tsx";
+
+
 function App() {
     return (
         <Router>
             <ScrollToTop />
             <div className="flex flex-col min-h-screen w-screen">
-                {/*  Header fijo arriba */}
-                 <Header />
+                <Header />
 
-                {/*  Contenido principal con padding superior */}
                 <main className="flex-grow pt-16">
                     <Routes>
+
+                        {/* 👉 Ruta para "/" */}
+                        <Route path="/" element={<Navigate to="/products" />} />
+
                         <Route path="/products" element={<Home />} />
                         <Route path="/sellers" element={<Sellers />} />
                         <Route path="/profile" element={<Profile />} />
                         <Route path="/create_post" element={<CreatePost />} />
                         <Route path="/create_crop" element={<CreateCrop />} />
                         <Route path="/my_products" element={<MyProducts />} />
-                        <Route path="/products-by-seller/:username" element={<ProductsBySeller />} />
+                        <Route path="/products-by-seller/:id" element={<ProductsBySeller />} />
                         <Route path="/product_details/:id" element={<ProductDetails />} />
                         <Route path="/edit_post/:id" element={<EditPost />} />
+                        <Route path="/alerts" element={<Alerts />} />
+                        <Route path="/moderador" element={<PanelDeModerador />} />
+
                     </Routes>
                 </main>
 
-                {/*  Footer al final */}
                 <Footer />
             </div>
         </Router>
--- Project/frontend/src/Components/Header.tsx
+++ Project/frontend/src/Components/Header.tsx
@@ -100,31 +100,59 @@ export const Header: React.FC = () => {
                         >
                             Mis productos y cultivos
                         </NavLink>
+
+                        <NavLink
+                        to="/alerts"
+                        className={({ isActive }) =>
+                            `text-sm font-[Inter] ${
+                            isActive
+                                ? "text-[#448502] font-semibold"
+                                : "text-[#171A1F] hover:text-[#3C7602]"
+                            }`
+                        }
+                        >
+                        Alertas
+                        </NavLink>
+
+
+
+                        <NavLink
+                            to="/moderador"
+                            className={({ isActive }) =>
+                                `text-sm font-[Inter] ${
+                                    isActive
+                                        ? "text-[#448502] font-semibold"
+                                        : "text-[#171A1F] hover:text-[#3C7602]"
+                                }`
+                            }
+                        >
+                            Moderador
+                        </NavLink>
+
+
                     </nav>
 
 
                 </div>
 
                 {/* Botón */}
-                <a href="http://localhost:8000/api/auth/google/login/?process=login">
-                    <button className="hidden sm:flex items-center justify-center px-4 py-2 h-9 rounded-md bg-[#448502] text-white text-sm font-medium hover:bg-[#3C7602] active:bg-[#2F5D01] font-[Inter] transition">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-4 h-4 mr-2"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M5 12h14M12 5l7 7-7 7"
-                            />
-                        </svg>
-                        Logout
-                    </button>
-                </a>
+                <button className="hidden sm:flex items-center justify-center px-4 py-2 h-9 rounded-md bg-[#448502] text-white text-sm font-medium hover:bg-[#3C7602] active:bg-[#2F5D01] font-[Inter] transition">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-4 h-4 mr-2"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M5 12h14M12 5l7 7-7 7"
+                        />
+                    </svg>
+                    Logout
+                </button>
 
                 {/* Menú móvil (futuro) */}
                 <div className="sm:hidden">
