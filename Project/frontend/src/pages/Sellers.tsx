ada090b12dea5a6a4575098f2b3deb7a109c29c4
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2025-12-13 22:27:17 -0500

    v2

--- Project/frontend/src/pages/Sellers.tsx
+++ Project/frontend/src/pages/Sellers.tsx
@@ -125,7 +125,6 @@ export const Sellers = () => {
         {/* 🔸 Ordenar por (default: Nombre) */}
         <div className="flex items-center gap-2">
           <span className="text-neutral-900 font-[Inter] font-medium text-base">
-            Sort by: Nombre
           </span>
         </div>
       </div>
